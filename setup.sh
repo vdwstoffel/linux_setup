a0ff80b5486be29dfffa1ac5f83574368d644ccf
Stoffel <86371768+vdwstoffel@users.noreply.github.com>
2023-09-17 13:31:06 +0200

    check if folder exists before installing

--- setup.sh
+++ setup.sh
@@ -14,16 +14,36 @@ else
 fi
 
 # install oh my zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing oh my zsh..."
+if [ ! -d "$HOME/.oh-my-zsh" ];
+then
+    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "oh my zsh already installed"
+fi
 
 # install plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "Installing plugins..."
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ];
+then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+    echo "zsh-autosuggestions already installed"
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ];
+then 
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+ echo "zsh-syntax-highlighting already installed"
+fi
 
 # make zsh the default shell
+echo "Making zsh the default shell..."
 chsh -s $(which zsh)
 
 
 # create symbolic links for the dotfiles
+echo "Creating symbolic links..."
 ln -sf $working_directory/.zshrc /home/$current_user/.zshrc
 ln -sf $working_directory/.gitconfig /home/$current_user/.gitconfig
